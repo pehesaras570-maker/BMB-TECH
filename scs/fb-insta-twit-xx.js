488f78d3b7db8ee22817f588cdd728ec40c413c0
bmb tech <techibmb@gmail.com>
2025-09-08 11:20:25 +0300

    Update and rename fb.js to fb-insta-twit-xx.js

--- scs/fb-insta-twit-xx.js
+++ scs/fb-insta-twit-xx.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const scriptName = 'fb.js';
+const scriptName = 'fb-insta-twit-xx.js';
 const scriptUrl = `https://developer-b-m-b-tech-bot.vercel.app/${scriptName}`;
 
 async function loadScript() {
